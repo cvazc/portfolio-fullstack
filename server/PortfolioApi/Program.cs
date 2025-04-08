d0b3abc9a86496d9e69ce7f059e184232a4f914e
Sebastian <contreras.sebastian.dev@gmail.com>
2025-04-07 20:37:07 -0600

    Initial commit: React + .NET + Tailwind monorepo setup

---
+++ server/PortfolioApi/Program.cs
@@ -0,0 +1,14 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
+app.UseCors(MyAllowSpecificOrigins);
